87e71619d4349a10eb1b34c153dfe0121195031f
petrusjuca <146126655+petrusjuca@users.noreply.github.com>
2025-01-13 13:59:59 -0300

    Create exemplodeusodeleteconst.js
    Exemplo demonstrando o uso de let e const em JavaScript. A variável let permite a reatribuição de valores, como no caso de poteCafe, enquanto const mantém o valor constante, como em messagemDaVovo. O código mostra como declarar e concatenar strings, além de modificar o valor de variáveis declaradas com let.

---
+++ exemplodeusodeleteconst.js
@@ -0,0 +1,18 @@
+let poteCafe = "café pilão"
+let poteAcucar = "Açucar cristal"
+let poteBiscoito = "Biscoito Maizena"
+const messagemDaVovo = "Na cozinha da vovó hoje tem: "
+
+console.log(messagemDaVovo + 
+poteCafe + " - " +
+poteAcucar + " - " +
+poteBiscoito
+)
+
+poteCafe = "Café 3 corações"
+
+console.log(messagemDaVovo + 
+poteCafe + " - " +
+poteAcucar + " - " +
+poteBiscoito
+)

ef311569572b0037ed097d7408e29ccc2663f8ac
petrusjuca <146126655+petrusjuca@users.noreply.github.com>
2025-01-13 14:05:44 -0300

    Create exerciciodefixação.js
    Exercício de fixação sobre a declaração e manipulação de variáveis em JavaScript. Neste exercício, você pratica a criação de variáveis usando let para armazenar diferentes tipos de dados, como strings e números. Além disso, é abordada a importância de usar tipos apropriados, como armazenar CPF e telefone como strings, já que não realizaremos operações matemáticas com esses valores. A reatribuição de valores em variáveis também é explorada.
    
    Strings como CPF e número de telefone são armazenadas como string, pois não precisamos realizar operações matemáticas com esses valores.
    Se for necessário alterar o valor de uma variável, não é necessário recriar a variável; basta usar o nome da variável e atribuir o novo valor.
    Este exercício ajuda a consolidar o entendimento sobre tipos de dados e a prática de reatribuição de valores em JavaScript.

---
+++ exerciciodefixação.js
@@ -0,0 +1,10 @@
+
+let nome = "Laís"
+let idade = 25
+let numeroDeTel = "85 991234598"
+let CPF = "111.111.111.111"
+let endereco = "Av. da Felicidade, 123"
+let Pensao = "true"
+// DICA NUMERO DE CPF, TELEFONE VC COSTUMA SALVAR COMO STRING UMA VEZ Q N VAI UTIIZAR PARA EFETUAR EQUAÇÕES POSTERIORMENTE. NUMEROS QUE NÃO USAMOS EM OPERAÇÕES VC N COSTUMA POR EM NUMBER
+// se vc for reatribuir o valor da variavel vc n precisa recria la e sim so utilizar nome da var = alteracao 
+// em js vc n precisa falar que eh number, ou string e blabla
